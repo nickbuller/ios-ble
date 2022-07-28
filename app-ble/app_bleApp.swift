4f27ae4e5ee49cb07c8e50c977a8d7accfff9a3e
Nick Buller <nbuller@eclinicalhealth.com>
2022-07-28 18:47:33 +0100

    Initial Commit

---
+++ app-ble/app_bleApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_bleApp.swift
+//  app-ble
+//
+//  Created by Nick Buller on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct app_bleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
